888faab496cd9dd9e778b4629a9029bcebc3127c
Reina <MatthewrBaron@gmail.com>
2020-04-16 17:41:04 -0400

    Email Address update

--- res/script/javascript.js
+++ res/script/javascript.js
@@ -7,7 +7,7 @@ function copyEmail() {
   var myEmail = document.createElement("textarea");
   document.body.appendChild(myEmail);
   //myEmail.style.display = "none";
-  myEmail.value = "matthewrbaron@gmail.com"; //change deadname email address
+  myEmail.value = "ReinaLalibertebaron@gmail.com"; //change deadname email address
   myEmail.select();
   myEmail.setSelectionRange(0, 99999);
   document.execCommand("copy");
