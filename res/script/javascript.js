e6ab1b9bf851424e42206bdd8329dffa08c79f34
aComputerWhitch <RainaRBaron@gmail.com>
2021-06-28 12:18:12 -0400

    fuction to modify all navigation bars

--- res/script/javascript.js
+++ res/script/javascript.js
@@ -26,7 +26,6 @@ if (currentTheme) {
         toggleSwitch.checked = true;
     }
 }
-
 function switchTheme(e) {
   console.log("flag B1");
     if (e.target.checked) {
@@ -39,3 +38,79 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+//=============================================================================
+function addNavBar() {
+  let elem = document.createElement('ul');
+  elem.setAttribute('class','navbar-nav')
+  elem.setAttribute('id','nav-ul-0');
+  let toAppend = document.getElementById("collapsibleNavbar");
+  toAppend.appendChild(elem);
+  //***
+  elem = document.createElement('li');
+  elem.setAttribute('class','nav-item');
+  elem.setAttribute('id','nav-li-index');
+  toAppend = document.getElementById("nav-ul-0");
+  toAppend.appendChild(elem);
+
+  elem = document.createElement('a');
+  elem.setAttribute('class','nav-link');
+  elem.setAttribute('id','nav-link-0');
+  elem.setAttribute('href','index.html');
+  elem.textContent = "Home";
+  toAppend = document.getElementById("nav-li-index");
+  toAppend.appendChild(elem);
+  //document.getElementById("nav-li-index").href = "index.html";
+
+
+  //***
+  elem = document.createElement('li');
+  elem.setAttribute('class','nav-item');
+  elem.setAttribute('id','nav-li-projects');
+  toAppend = document.getElementById("nav-ul-0");
+  toAppend.appendChild(elem);
+
+  elem = document.createElement('a');
+  elem.setAttribute('class','nav-link');
+  elem.setAttribute('id','nav-link-1');
+  elem.setAttribute('href','projects.html');
+  elem.textContent = "Projects";
+  toAppend = document.getElementById("nav-li-projects");
+  toAppend.appendChild(elem);
+  document.getElementById("nav-li-projects").href = "projects.html";
+
+
+
+  //***
+  elem = document.createElement('li');
+  elem.setAttribute('class','nav-item');
+  elem.setAttribute('id','nav-li-about');
+  element = document.getElementById("nav-ul-0");
+  element.appendChild(elem);
+
+  elem = document.createElement('a');
+  elem.setAttribute('class','nav-link');
+  elem.setAttribute('id','nav-link-2');
+  elem.setAttribute('href','about.html');
+  elem.textContent = "About Me";
+  element = document.getElementById("nav-li-about");
+  element.appendChild(elem);
+  document.getElementById("nav-li-about").href = "about.html";
+
+
+
+  //***
+  elem = document.createElement('li');
+  elem.setAttribute('class','nav-item');
+  elem.setAttribute('id','nav-li-resume');
+  element = document.getElementById("nav-ul-0");
+  element.appendChild(elem);
+
+  elem = document.createElement('a');
+  elem.setAttribute('class','nav-link');
+  elem.setAttribute('id','nav-link-3');
+  elem.setAttribute('href','resume.html');
+  elem.textContent = "Resume";
+  element = document.getElementById("nav-li-resume");
+  element.appendChild(elem);
+  document.getElementById("nav-li-resume").href = "resume.html";
+}
